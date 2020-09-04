5f8a76375fc546b33ad184a6484189b95d69c4c8
renisalcedo <salcedoreynaldo5@gmail.com>
2020-09-04 17:18:12 -0400

    Initial server push

--- server/README.md
+++ server/README.md
@@ -1,1 +1,10 @@
-# Server 
+# Tutorin Server
+
+## Getting Started
+1. Get SDK Ready https://cloud.google.com/sdk/docs
+2. Install Dependencies: pip install -r requirements.txt
+
+## Running locally
+To run locally, you need to use gunicorn with the ``flask_socket`` worker:
+
+    $ gunicorn -b 127.0.0.1:8080 -k flask_sockets.worker main:app
---
+++ server/main.py
@@ -0,0 +1,34 @@
+import logging
+# from controllers.chat_controller import ChatController
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+# Initializes the class routes
+@sockets.route('/chat')
+def chat_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        if message is None:  # message is "None" if the client has closed.
+            continue
+        # Send the message to all clients connected to this webserver
+        # process. (To support multiple processes or instances, an
+        clients = ws.handler.server.clients.values()
+        for client in clients:
+            client.ws.send(message)
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
