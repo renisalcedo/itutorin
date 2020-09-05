5a758a615acffab7aa95638a0a45c14023343844
Asel <peirisasel373@gmail.com>
2020-09-05 21:31:32 +0530

    Setting routes and chat layout

---
+++ client/src/Pages/ChatLayout/Content/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div>Content</div>
+      </>
+    );
+  }
+}
+
+export default Content;
---
+++ client/src/Pages/ChatLayout/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div>Sidebar</div>
+      </>
+    );
+  }
+}
+
+export default Sidebar;
