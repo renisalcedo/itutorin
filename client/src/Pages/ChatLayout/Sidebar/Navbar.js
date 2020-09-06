56ffc8556b7889f1804d989a0fa96a5ad8f0946d
Asel <peirisasel373@gmail.com>
2020-09-06 16:45:04 +0530

    minor changes

--- client/src/Pages/ChatLayout/Sidebar/Navbar.js
+++ client/src/Pages/ChatLayout/Sidebar/Navbar.js
@@ -16,7 +16,6 @@ function Navbar() {
 
   return (
     <>
-<<<<<<< HEAD
       <nav className="nav-menu-active">
         <ul className="nav-menu-items">
           <Button
@@ -31,24 +30,6 @@ function Navbar() {
             style={{ margin: "10px", padding: "5px", width: "18rem" }}
           >
             Create Flashcard
-=======
-      <IconContext.Provider value={{ color: "#fff" }}></IconContext.Provider>
-
-
-      <nav className="nav-menu-active">
-        <ul className="nav-menu-items">
-          {SidebarData.map((item, index) => {
-            return (
-              <Button variant="outline-info" style={{margin:"10px",padding:"2px",width:"95%",border:"none"}}>
-                <li key={index} className={item.cname}>
-                  {item.icon} {item.title}
-                </li>
-              </Button>
-            );
-          })}
-          <Button className="quiz" variant="info">
-            Quiz
->>>>>>> d2f6151dfbd15af14013c65fb0c50a0899a364e2
           </Button>
           );
         </ul>
@@ -59,6 +40,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-
-
