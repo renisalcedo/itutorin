2eba86d512e9ac212878ec64d626f416d8953a30
Michael Mathew <michael@Michaels-MacBook-Pro.local>
2020-09-05 20:45:16 -0400

    Changes to user controller functions, added testes in main.py

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -11,29 +11,56 @@ class UserController:
         if self._user_exist(user_query):
             return user_query
 
-    def create_user(self, user, data):
+    def create_user(self, user,session_id):
         # TODO
-        """" MOST OF THE WORK HERE COMPLETE. Make sure user and user data in parameters get store """
-        key = self._datastore_client.key('Users')
-        users = datastore.Entity(key=key)
-        users.update({ 'user': user })
+        #Need Unique usernames
+        #MOST OF THE WORK HERE COMPLETE. Make sure user and user data in parameters get store
+        users = self._datastore_client.query(kind='Users')
+        user_query = users.add_filter('name', '=', user).fetch()
+        indexU=self.get_user(user)
+        if indexU:
+        	print('user with that name already exists')
+        else:
+        
+        
+        	key = self._datastore_client.key('Users')
+        	users = datastore.Entity(key=key)
+        	#users.update({ 'user': user })
+        	users.update({ 'user': user , 'session_id': session_id})
 
-        self._datastore_client.put(users)
+        	self._datastore_client.put(users)
+        	
 
     def get_user(self,user):
         # TODO
-        """ WE WANT TO GET DATA FOR A SPECIFIC USER IN OUR DATASTORE"""
+        #WE WANT TO GET DATA FOR A SPECIFIC USER IN OUR DATASTORE
+        return list(self._datastore_client.query(kind='Users').add_filter('user', '=', user).fetch())
+        
+        
+        
+        
         pass
 
     def get_user_session(self,user):
         # TODO
-        """ FOR A SPEFIC USER ON PARAMETER GET THE SESSION AND RETURN IT """
+        #FOR A SPEFIC USER ON PARAMETER GET THE SESSION AND RETURN IT 
+        indexU=self.get_user(user)
+        if indexU:
+        	return indexU[0]['session_id']
+        
         pass
 
     def set_user_session(self,user,session_id):
         # TODO
-        """ UPDATE THE EXISTING USER DATA TO HAVE THE PARAMETER ASSIGNED SESSION_ID """
+        #UPDATE THE EXISTING USER DATA TO HAVE THE PARAMETER ASSIGNED SESSION_ID 
+        indexU=self.get_user(user)
+        if indexU:
+        	indexU[0]['session_id']=session_id
+        	self._datastore_client.put(indexU[0])
+        	
+        
         pass
+        
 
     def _user_exist(self,query):
         return len(list(query)) > 0
--- server/main.py
+++ server/main.py
@@ -4,11 +4,13 @@ from flask_restful import Resource,Api
 from flask_sockets import Sockets
 import logging
 
+
 # FIXES ISSUE FOR DATASTORE AND SOCKET TOGETHER
 grpc_gevent.init_gevent()
 
 # Imported Controllers
 from controllers.chat_controller import ChatController
+from controllers.user_controller import UserController
 
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -26,7 +28,24 @@ def server_error(e):
 """ ROUTE CONTROLLERS SECTION """
 """ ------------------------- """
 api.add_resource(ChatController, '/sessions')
+userC=UserController()
+userC.create_user('James Mathew','3526268')
 
+print('user login ')
+print(userC.login('James Mathew'))
+userC.get_user('James Mathew')
+
+print('user session before ')
+print(userC.get_user_session('James Mathew'))
+userC.set_user_session('James Mathew','0009')
+
+print('user session after set new usersession ')
+print(userC.get_user_session('James Mathew'))
+
+print()
+userC=UserController()
+print("trying to add user same name")
+userC.create_user('James Mathew','3526268')
 """ -------------------- """
 """ CHAT SOCKET SECTION  """
 """ -------------------- """
